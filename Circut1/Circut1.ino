884120fa46da49918e6535f29661d0aab537b424
Bradley <47512745+bgrose@users.noreply.github.com>
2020-02-28 10:12:50 -0500

    Update Circut1.ino

--- Circut1/Circut1.ino
+++ Circut1/Circut1.ino
@@ -1,45 +1,45 @@
 // Output pins.
-static int output[] = {0,1,2,3};
+static int output[] = {0, 1, 2, 3};
 // 8421 code lookup table.
 static int segs_encoding[][4] = {
- {0,0,0,0},
-{0,0,0,1},
-{0,0,1,0},
-{0,0,1,1},
-{0,1,0,0},
-{0,1,0,1},
-{0,1,1,0},
-{0,1,1,1},
-{1,0,0,0},
-{1,0,0,1}
+  {0, 0, 0, 0},
+  {0, 0, 0, 1},
+  {0, 0, 1, 0},
+  {0, 0, 1, 1},
+  {0, 1, 0, 0},
+  {0, 1, 0, 1},
+  {0, 1, 1, 0},
+  {0, 1, 1, 1},
+  {1, 0, 0, 0},
+  {1, 0, 0, 1}
 };
 // Outputs an 8421 decimal digit on the output pins from
 // base to base + 3 in big endian order.
 void show(int base, int num) {
-for (int i = 0; i < 4; ++i) {
-if(segs_encoding[num][i] == 1) {
-digitalWrite(base + 3 - i , HIGH);
-} else {
-digitalWrite(base + 3 - i , LOW);
-}
-}
+  for (int i = 0; i < 4; ++i) {
+    if (segs_encoding[num][i] == 1) {
+      digitalWrite(base + 3 - i , HIGH);
+    } else {
+      digitalWrite(base + 3 - i , LOW);
+    }
+  }
 }
 // Output a decimal value.
 void show_decimal(int num) {
-show(0, num);
+  show(0, num);
 }
 // the setup routine runs once when you press reset:
 void setup() {
-for (int i = 0; i < 4; ++i)
-pinMode(i, OUTPUT);
+  for (int i = 0; i < 4; ++i) //Adjusted Pin values for Communications
+    pinMode(i, OUTPUT);
 }
 // the loop routine runs over and over again forever:
 void loop() {
-static int i = 0;
-show_decimal(i);
-delay(1000);
-// Increases the counter.
-++i;
-if (i == 10)
-i = 0;
+  static int i = 0;
+  show_decimal(i);
+  delay(1000);
+  // Increases the counter.
+  ++i;
+  if (i == 10)
+    i = 0;
 }
